b28aedf81a4418f81371401c00d09918f5d6e5c3
srisreenivas <sgovada@innominds.com>
2026-02-05 18:20:17 +0530

    new files

---
+++ src/main/java/com/oauth/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.oauth.controller;
+
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/profile")
+    public String profile(@AuthenticationPrincipal OAuth2User user,
+                           Model model) {
+
+        model.addAttribute("name", user.getAttribute("name"));
+        model.addAttribute("email", user.getAttribute("email"));
+        model.addAttribute("picture", user.getAttribute("picture"));
+
+        return "profile";
+    }
+}
+
